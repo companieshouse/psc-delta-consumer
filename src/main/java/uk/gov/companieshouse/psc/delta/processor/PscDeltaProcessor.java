519a0fd62ef137e3a2ab4a8c9822d0025dc96b43
lbedrosyanCH <lbedrosyan@companieshouse.gov.uk>
2023-10-10 08:58:47 +0100

    Added structured logging. DSND-1956

--- src/main/java/uk/gov/companieshouse/psc/delta/processor/PscDeltaProcessor.java
+++ src/main/java/uk/gov/companieshouse/psc/delta/processor/PscDeltaProcessor.java
@@ -3,16 +3,16 @@ package uk.gov.companieshouse.psc.delta.processor;
 import static java.lang.String.format;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import consumer.exception.NonRetryableErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.delta.Psc;
 import uk.gov.companieshouse.api.delta.PscDelta;
 import uk.gov.companieshouse.api.psc.FullRecordCompanyPSCApi;
 import uk.gov.companieshouse.delta.ChsDelta;
 import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.psc.delta.logging.DataMapHolder;
 import uk.gov.companieshouse.psc.delta.service.api.ApiClientService;
 import uk.gov.companieshouse.psc.delta.transformer.PscApiTransformer;
 
@@ -38,34 +38,36 @@ public class PscDeltaProcessor {
      * Process PSC Statement Delta message.
      */
     public void processDelta(Message<ChsDelta> chsDelta) {
+
         final ChsDelta payload = chsDelta.getPayload();
         final String contextId = payload.getContextId();
 
-        logger.info(format("Successfully extracted Chs Delta with context_id %s",
-                payload.getContextId()));
-        ObjectMapper mapper = new ObjectMapper();
+        logger.info(format("Successfully extracted Chs Delta with context_id %s", contextId));
+
         PscDelta pscDelta;
         try {
-            pscDelta = mapper.readValue(payload.getData(),
-                    PscDelta.class);
-            logger.trace(format("Successfully extracted psc delta of %s",
-                    pscDelta.toString()));
+            pscDelta = new ObjectMapper().readValue(payload.getData(), PscDelta.class);
+            Psc psc = pscDelta.getPscs().get(0); // We will only ever get one PSC per request
+
+            DataMapHolder.get()
+                    .companyNumber(psc.getCompanyNumber())
+                    .itemId(psc.getPscId());
+
+            logger.infoContext(contextId, "Successfully extracted psc delta",
+                    DataMapHolder.getLogMap());
         } catch (Exception ex) {
             logger.error(format(ex.getMessage()));
-            throw new NonRetryableErrorException(
-                    "Error when extracting psc delta", ex);
+            throw new NonRetryableErrorException("Error when extracting psc delta", ex);
         }
 
         FullRecordCompanyPSCApi fullRecordCompanyPscApi;
-
         try {
             fullRecordCompanyPscApi = transformer.transform(pscDelta);
-            logger.info(format("Successfully transformed psc. Api object: %s",
-                    fullRecordCompanyPscApi));
+            logger.infoContext(contextId, "Successfully transformed psc",
+                    DataMapHolder.getLogMap());
         } catch (Exception ex) {
             logger.error(format(ex.getMessage()));
-            throw new NonRetryableErrorException(
-                    "Error when transforming into api object", ex);
+            throw new NonRetryableErrorException("Error when transforming into api object", ex);
         }
 
         apiClientService.putPscFullRecord(contextId,
