a3fe00f31fd304f12504cfa0ea3e5aae88335b90
crowlandsCH <crowlands@companieshouse.gov.uk>
2024-12-05 19:16:50 +0000

    DSND-3113: Wildcard imports fixed

--- src/test/java/uk/gov/companieshouse/psc/delta/service/api/ApiClientServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/psc/delta/service/api/ApiClientServiceImplTest.java
@@ -19,7 +19,10 @@ import uk.gov.companieshouse.psc.delta.processor.DeletePscApiClientRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
