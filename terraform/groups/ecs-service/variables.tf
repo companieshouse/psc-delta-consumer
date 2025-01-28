07288b99266ddfb44c4591684d859c93f817982f
nghessen-ch <nghessen@companieshouse.gov.uk>
2025-01-28 10:30:12 +0000

    adjusting tf code and adding cidev schedule

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -73,12 +73,12 @@ variable "service_autoscale_target_value_cpu" {
 variable "service_autoscale_scale_in_cooldown" {
   type        = number
   description = "Cooldown in seconds for ECS Service scale in (run fewer tasks)"
-  default     = 300
+  default     = 600
 }
 variable "service_autoscale_scale_out_cooldown" {
   type        = number
   description = "Cooldown in seconds for ECS Service scale out (add more tasks)"
-  default     = 300
+  default     = 600
 }
 
 variable "service_scaledown_schedule" {
